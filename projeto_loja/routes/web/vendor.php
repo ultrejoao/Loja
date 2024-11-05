cddd54f9a7b0abb8a2f4bc14506a7a5e981deebb
Ultrejoao <engs-joaohass@camporeal.edu.br>
2024-11-05 17:42:09 -0300

    login atualizado2

---
+++ projeto_loja/routes/web/vendor.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\Backend\VendorController;
+use Illuminate\Support\Facades\Route;
+
+//rota vendedor
+Route::get('vendor/dashboard', [VendorController::class, 'dashboard'])
+->middleware(['auth', 'vendor'])
+->name('vendor.dashboard');
